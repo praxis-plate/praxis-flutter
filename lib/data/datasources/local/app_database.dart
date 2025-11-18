88b23ffe5838dca30640ad9f63668389fcd657d2
chxrus <leftchxrus@gmail.com>
2025-11-19 02:56:21 +0300

    [COD-3] Добавил таблицы БД и подключение, а также тесты

---
+++ lib/data/datasources/local/app_database.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+import '../../../core/config/env_config.dart';
+import 'drift_tables.dart';
+
+part 'app_database.g.dart';
+
+@DriftDatabase(tables: [PdfBooks, Bookmarks, Explanations])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase() : super(_openConnection());
+
+  AppDatabase.forTesting(super.executor);
+
+  @override
+  int get schemaVersion => 1;
+
+  @override
+  MigrationStrategy get migration {
+    return MigrationStrategy(
+      onCreate: (Migrator m) async {
+        await m.createAll();
+      },
+      beforeOpen: (details) async {
+        await customStatement('PRAGMA foreign_keys = ON');
+      },
+    );
+  }
+
+  static QueryExecutor _openConnection() {
+    return LazyDatabase(() async {
+      final dbFolder = await getApplicationDocumentsDirectory();
+      final file = File(p.join(dbFolder.path, EnvConfig.dbPath));
+      return NativeDatabase(file);
+    });
+  }
+}
