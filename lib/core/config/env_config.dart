3189cb7cf25e7f3e61bb2b37c04c62bfed714084
Arkady Lukovkin <lukovkin.ark@gmail.com>
2025-11-19 02:32:08 +0300

    [COD-1] Проинциализировал зависимости

---
+++ lib/core/config/env_config.dart
@@ -0,0 +1,15 @@
+import 'package:envied/envied.dart';
+
+part 'env_config.g.dart';
+
+@Envied(path: '.env')
+abstract class EnvConfig {
+  @EnviedField(varName: 'GEMINI_API_KEY', obfuscate: true)
+  static final String geminiApiKey = _EnvConfig.geminiApiKey;
+
+  @EnviedField(varName: 'SEARCH_API_KEY', obfuscate: true)
+  static final String searchApiKey = _EnvConfig.searchApiKey;
+
+  @EnviedField(varName: 'SEARCH_ENGINE_ID', obfuscate: true)
+  static final String searchEngineId = _EnvConfig.searchEngineId;
+}
