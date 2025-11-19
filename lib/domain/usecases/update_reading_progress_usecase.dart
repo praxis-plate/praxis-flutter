36b7382999abbd42435c687b9bbeccdf60349f52
Arkady Lukovkin <lukovkin.ark@gmail.com>
2025-11-19 12:06:05 +0300

    [COD-8] Написал юзкейсы и тесты

---
+++ lib/domain/usecases/update_reading_progress_usecase.dart
@@ -0,0 +1,29 @@
+import 'package:codium/domain/repositories/pdf_repository.dart';
+import 'package:get_it/get_it.dart';
+import 'package:talker_flutter/talker_flutter.dart';
+
+class UpdateReadingProgressUseCase {
+  final IPdfRepository _pdfRepository;
+
+  UpdateReadingProgressUseCase(this._pdfRepository);
+
+  Future<void> execute({
+    required String bookId,
+    required int currentPage,
+  }) async {
+    try {
+      if (bookId.isEmpty) {
+        throw ArgumentError('Book ID cannot be empty');
+      }
+
+      if (currentPage < 0) {
+        throw ArgumentError('Current page must be non-negative');
+      }
+
+      await _pdfRepository.updateReadingProgress(bookId, currentPage);
+    } catch (e, st) {
+      GetIt.I<Talker>().handle(e, st);
+      rethrow;
+    }
+  }
+}
