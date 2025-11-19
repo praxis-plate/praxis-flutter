8c8775972806cfa56c223fa438a3eeccb44df0e6
chxrus <leftchxrus@gmail.com>
2025-11-19 03:04:32 +0300

    [COD-4] Поправил модели

--- lib/domain/models/ai_explanation/explanation.dart
+++ lib/domain/models/ai_explanation/explanation.dart
@@ -2,14 +2,6 @@ import 'package:codium/domain/models/ai_explanation/search_source.dart';
 import 'package:equatable/equatable.dart';
 
 class Explanation extends Equatable {
-  final String id;
-  final String pdfBookId;
-  final int pageNumber;
-  final String selectedText;
-  final String explanation;
-  final List<SearchSource> sources;
-  final DateTime createdAt;
-
   const Explanation({
     required this.id,
     required this.pdfBookId,
@@ -19,6 +11,14 @@ class Explanation extends Equatable {
     this.sources = const [],
     required this.createdAt,
   });
+
+  final String id;
+  final String pdfBookId;
+  final int pageNumber;
+  final String selectedText;
+  final String explanation;
+  final List<SearchSource> sources;
+  final DateTime createdAt;
 
   Explanation copyWith({
     String? id,
@@ -40,17 +40,45 @@ class Explanation extends Equatable {
     );
   }
 
-  @override
-  List<Object?> get props => [
-    id,
-    pdfBookId,
-    pageNumber,
-    selectedText,
-    explanation,
-    sources,
-    createdAt,
-  ];
+  factory Explanation.fromJson(Map<String, dynamic> json) {
+    return Explanation(
+      id: json['id'],
+      pdfBookId: json['pdfBookId'],
+      pageNumber: json['pageNumber'],
+      selectedText: json['selectedText'],
+      explanation: json['explanation'],
+      sources: List<SearchSource>.from(
+        json['sources']?.map((x) => SearchSource.fromJson(x)),
+      ),
+      createdAt: DateTime.fromMillisecondsSinceEpoch(json['createdAt']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'pdfBookId': pdfBookId,
+      'pageNumber': pageNumber,
+      'selectedText': selectedText,
+      'explanation': explanation,
+      'sources': sources.map((x) => x.toJson()).toList(),
+      'createdAt': createdAt.millisecondsSinceEpoch,
+    };
+  }
 
   @override
-  bool get stringify => true;
+  bool? get stringify => true;
+
+  @override
+  List<Object> get props {
+    return [
+      id,
+      pdfBookId,
+      pageNumber,
+      selectedText,
+      explanation,
+      sources,
+      createdAt,
+    ];
+  }
 }
