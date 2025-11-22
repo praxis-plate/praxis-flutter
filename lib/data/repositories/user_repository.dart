34c8dc9df359274bf4d635356520d1368c331c35
chxrus <leftchxrus@gmail.com>
2025-11-22 18:46:18 +0300

    [COD-32] Убрал неиспользуемый датасурс

--- lib/data/repositories/user_repository.dart
+++ lib/data/repositories/user_repository.dart
@@ -1,21 +1,19 @@
 import 'package:codium/core/exceptions/user_exceptions.dart';
 import 'package:codium/data/datasources/local/local_auth_datasource.dart';
 import 'package:codium/domain/datasources/abstract_auth_datasource.dart';
-import 'package:codium/domain/datasources/abstract_user_datasource.dart';
 import 'package:codium/domain/models/models.dart';
 import 'package:codium/domain/repositories/abstract_user_repository.dart';
 
 class UserRepository implements IUserRepository {
-  final IUserDataSource _dataSource;
   final IAuthDataSource _authDataSource;
 
-  UserRepository(this._dataSource, this._authDataSource);
+  UserRepository(this._authDataSource);
 
   @override
   Future<User> getCurrentUser() async {
     try {
       if (_authDataSource is LocalAuthDataSource) {
-        final localAuthDataSource = _authDataSource as LocalAuthDataSource;
+        final localAuthDataSource = _authDataSource;
         final user = await localAuthDataSource.getCurrentUser();
         if (user == null) {
           throw UserException('No authenticated user found');
