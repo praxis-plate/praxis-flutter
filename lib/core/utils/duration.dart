15043bc817fb4b4deb8895895e879ee9fcbfadc7
chxrus <leftchxrus@gmail.com>
2025-12-07 15:30:31 +0300

    [COD-42] Добавил класс для форматирований

--- lib/core/utils/duration.dart
+++ lib/core/utils/duration.dart
@@ -1,3 +1,34 @@
+import 'package:codium/l10n/app_localizations.dart';
+
+class DurationFormatter {
+  static String formatCompact(Duration duration, AppLocalizations s) {
+    final hours = duration.inHours;
+    final minutes = duration.inMinutes.remainder(60);
+    return '$hours${s.hoursShort} $minutes${s.minutesShort}';
+  }
+
+  static String formatEstimated(Duration duration, AppLocalizations s) {
+    if (duration.inSeconds < 60) {
+      return '${duration.inSeconds} ${s.minutes}';
+    } else if (duration.inMinutes < 60) {
+      return '${duration.inMinutes} ${s.minutes}';
+    } else {
+      return '${duration.inHours} ${s.hours}';
+    }
+  }
+
+  static String formatLessonDuration(Duration duration, AppLocalizations s) {
+    final minutes = duration.inMinutes;
+    if (minutes < 60) {
+      return '$minutes ${s.minutesShort}';
+    }
+    final hours = duration.inHours;
+    final remainingMinutes = minutes.remainder(60);
+    return '$hours${s.hoursShort} $remainingMinutes${s.minutesShort}';
+  }
+}
+
+@Deprecated('Use DurationFormatter methods with localization instead')
 extension DurationFormatting on Duration {
   String format() {
     String twoDigits(int n) => n.toString().padLeft(2, '0');
