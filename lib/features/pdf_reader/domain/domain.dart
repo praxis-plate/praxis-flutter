9ffd1f6637247833ee2a09b919ceab8c41386b28
Arkady Lukovkin <lukovkin.ark@gmail.com>
2026-01-09 20:17:14 +0300

    [CDM-6] Удалил экран чтения

--- lib/features/pdf_reader/domain/domain.dart
+++
@@ -1,2 +0,0 @@
-export 'pdf_cache_service.dart';
-export 'pdf_rendering_config.dart';
--- lib/features/pdf_reader/pdf_reader.dart
+++
@@ -1,4 +0,0 @@
-export 'bloc/bloc.dart';
-export 'domain/domain.dart';
-export 'view/view.dart';
-export 'widgets/widgets.dart';
