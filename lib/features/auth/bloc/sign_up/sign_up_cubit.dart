c1bf3b15ff98416c5e9c235d4b5c36d8551570a2
chxrus <leftchxrus@gmail.com>
2026-01-09 17:10:41 +0300

    [CDM-4] Заменил текст ошибки на код, убрал в локальное состояние скрытие текста

--- lib/features/auth/bloc/sign_up/sign_up_cubit.dart
+++ lib/features/auth/bloc/sign_up/sign_up_cubit.dart
@@ -1,13 +1,33 @@
+import 'dart:async';
+
+import 'package:codium/core/bloc/auth/auth_bloc.dart';
+import 'package:codium/core/error/error.dart';
 import 'package:codium/core/validators/email_validator.dart';
 import 'package:codium/core/validators/password_validator.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:formz/formz.dart';
+import 'package:get_it/get_it.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 part 'sign_up_state.dart';
 
 class SignUpCubit extends Cubit<SignUpState> {
-  SignUpCubit() : super(const SignUpState());
+  SignUpCubit({required this.authBloc}) : super(const SignUpState()) {
+    streamSubscription = authBloc.stream.listen((AuthState authState) {
+      if (authState is AuthErrorState) {
+        emit(
+          state.copyWith(
+            status: FormzSubmissionStatus.failure,
+            errorCode: authState.errorCode,
+          ),
+        );
+      }
+    });
+  }
+
+  final AuthBloc authBloc;
+  late final StreamSubscription streamSubscription;
 
   void emailChanged(String value) {
     final email = EmailValidator.dirty(value);
@@ -33,9 +53,11 @@ class SignUpCubit extends Cubit<SignUpState> {
     emit(state.copyWith(status: FormzSubmissionStatus.inProgress));
   }
 
-  void togglePasswordVisibility() {
-    emit(state.copyWith(obscurePassword: !state.obscurePassword));
-  }
-
   void reset() => emit(state.copyWith(status: FormzSubmissionStatus.initial));
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    GetIt.I<Talker>().handle(error, stackTrace);
+    super.onError(error, stackTrace);
+  }
 }
